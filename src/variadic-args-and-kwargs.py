ad455a6ad5cf9cb549aa6528d2ca676fed5d0c79
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2024-06-14 08:57:45 -0700

    Add kwargs

--- src/variadic-args-and-kwargs.py
+++ src/variadic-args-and-kwargs.py
@@ -11,6 +11,7 @@ def fixed_args():
 
 
 def variadic_args(*args):
+    print(f"args: {args}")
     print(f"*args: {args}")
     print("done")
 
@@ -18,6 +19,16 @@ def variadic_args(*args):
 def fixed_and_variadic_args(first, *args):
     print(f"*args: {args}")
     print("done")
+
+
+def fixed_and_variadic_kwargs(first, **kwargs):
+    print(f"**kwargs: {kwargs}")
+    print("done")
+
+
+def wrapper_function(first, *args, **kwargs):
+    fixed_and_variadic_args(first, *args)
+    fixed_and_variadic_kwargs(first, **kwargs)
 
 
 if __name__ == "__main__":
@@ -39,3 +50,12 @@ if __name__ == "__main__":
         print(f"{type(e).__name__}: {e}")
     fixed_and_variadic_args(1)
     fixed_and_variadic_args(1, 2, 3)
+
+    wrapper_function(1)
+    wrapper_function(1, 2)
+
+    # correct way to pass variadic kwargs:
+    wrapper_function(1, 2, third=3)
+
+    # INCORRECT way to pass variadic kwargs:
+    wrapper_function(1, 2, {"third": 3})
