3d52ddb86585a08952e987eaeacd1998980d67ad
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2025-01-22 08:29:09 -0800

    Create top-k-frequent-words.py

---
+++ src/leetcode/top-k-frequent-words.py
@@ -0,0 +1,21 @@
+"""
+# Top K Frequent Words
+
+Reference: leetcode problem 692
+
+Given an array of strings words and an integer k, return the k most frequent strings.
+
+Return the answer sorted by the frequency from highest to lowest. Sort the words with
+the same frequency by their lexicographical order.
+
+Notes:
+    - Apparently a priority queue is a good way to solve this
+
+"""
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        pass
+
