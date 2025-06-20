2dcba3a3122e3302ffa9432a3e3d1b0662f11a4d
Ahmad, Nayef <nayef.ahmad.11@gmail.com>
2025-06-19 17:15:54 -0700

    Add Boyer-Moore algo for majority element

--- src/leetcode/majority-element.py
+++ src/leetcode/majority-element.py
@@ -37,8 +37,31 @@ class Solution:
         majority_element = top_nums[max_val]
         return majority_element
 
+    def majority_boyer_moore(self, nums: List[int]) -> int:
+        """
+        Reference: https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm  # noqa
+        Roughly, the idea is that the count of a candidate hits zero when it has been
+        "counterbalanaced" by a different element (or combination of elements). So we
+        can then reset `count` to zero. And then we start the process again.
+        """
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count = 1
+            elif candidate == num:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
+
+
 
 if __name__ == '__main__':
     input = [3, 2, 3]
     s = Solution()
     x = s.majorityElement(input)
+    x2 = s.majority_boyer_moore(input)
+    print('done')
